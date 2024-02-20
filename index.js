8a68de4352145d778357cec8d5da1b1d1ac2a729
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-02-20 18:28:19 -0400

    Public folder

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.listen(3000,()=>{
+    console.log("Listening port 3000");
+})
