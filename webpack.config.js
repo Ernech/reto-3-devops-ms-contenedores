13788a68439f0b11ee28c8f8182b98efd5d169e5
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-03-08 19:47:30 -0400

    Updating webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,30 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),  
     filename: 'bundle.js'
+  },
+  performance: {
+    maxEntrypointSize: 244000,
+    maxAssetSize: 244000
+},
+  mode:"production",
+  resolve:{
+     fallback: {
+    "fs": false,
+    "tls": false,
+    "net": false,
+    "path": false,
+    "zlib": false,
+    "http": false,
+    "https": false,
+    "stream": false,
+    "crypto": false,
+    "util":false,
+    "async_hooks":false,
+    "querystring":false,
+    "string_decoder":false,
+    "url":false,
+    "buffer":false,
+    "os":false
+  } 
   }
 };
